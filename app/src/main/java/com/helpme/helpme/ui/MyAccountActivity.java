b45a02c8c8c505e197bd8b4a3d370232089b0d25
arieftb <turbagus14@gmail.com>
2017-05-16 12:38:36 +0700

    LoginAcitivity Done Without Testing

---
+++ app/src/main/java/com/helpme/helpme/ui/MyAccountActivity.java
@@ -0,0 +1,15 @@
+package com.helpme.helpme.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.helpme.helpme.R;
+
+public class MyAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_account);
+    }
+}
