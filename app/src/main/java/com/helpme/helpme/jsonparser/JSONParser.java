c2a097bee46a4344535e3b5d2e13ab1f7475a70b
arieftb <turbagus14@gmail.com>
2017-05-16 08:54:17 +0700

    JSON Parse Session Manager and Replace Logo App

--- app/src/main/java/com/helpme/helpme/jsonparser/JSONParser.java
+++ app/src/main/java/com/helpme/helpme/jsonparser/JSONParser.java
@@ -6,7 +6,10 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BufferedHeader;
 import org.json.JSONException;
@@ -66,19 +69,70 @@ public class JSONParser {
         }
 
 
+        try {
+            mJSONObject = new JSONObject(json);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+
         return mJSONObject;
     }
 
-//    public JSONObject makeHttpRequest(String url, String method, List<NameValuePair> params){
-//
-//        try{
-//
-//            if (method == "POST"){
-//
-//            }
-//        }
-//
-//    }
+    public JSONObject makeHttpRequest(String url, String method, List<NameValuePair> params){
+
+        try{
+
+            if (method == "POST"){
+                DefaultHttpClient mDefaultHttpClient = new DefaultHttpClient();
+                HttpPost mHttpPost = new HttpPost(url);
+                mHttpPost.setEntity(new UrlEncodedFormEntity(params));
+
+                HttpResponse mHttpResponse = mDefaultHttpClient.execute(mHttpPost);
+                HttpEntity mHttpEntity = mHttpResponse.getEntity();
+                mInputStream = mHttpEntity.getContent();
+            } else if (method == "GET"){
+                DefaultHttpClient mDefaultHttpClient = new DefaultHttpClient();
+                String paramString = URLEncodedUtils.format(params, "utf-8");
+                url += "?" + paramString;
+                HttpGet mHttpGet = new HttpGet(url);
+
+                HttpResponse mHttpResponse = mDefaultHttpClient.execute(mHttpGet);
+                HttpEntity mHttpEntity = mHttpResponse.getEntity();
+                mInputStream = mHttpEntity.getContent();
+            }
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader mBufferedReader = new BufferedReader(new InputStreamReader(
+                    mInputStream, "iso-8859-1"),8);
+            StringBuilder mStringBuilder = new StringBuilder();
+            String line = null;
+            while ((line = mBufferedReader.readLine()) != null){
+                mStringBuilder.append(line + "\n");
+            }
+            mInputStream.close();
+            json = mStringBuilder.toString();
+
+        } catch (IOException e) {
+            Log.e("Buffer Error", "Error Converting Result " + e.toString());
+        }
+
+        try {
+            mJSONObject = new JSONObject(json);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+        return mJSONObject;
+    }
 
 
 
